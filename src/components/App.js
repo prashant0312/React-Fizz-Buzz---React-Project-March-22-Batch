0a496b337cccba0057be4c9c314de462d5451e70
Prashant Madhukar Kale <61052188+prashant0312@users.noreply.github.com>
2022-10-02 12:24:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,37 +5,43 @@ const App = () => {
 
   const [getData, setData] = useState(1);
 
-  const onClickIncrementHandler = () =>{
+  const onClickIncrementHandler = () => {
     setData(getData + 1);
-  }
-
-  const onClickDecrementHandler = () =>{
-    setData(getData - 1);
-  }
+  };
   
+  const onClickDecrementHandler = () => {
+    setData(getData - 1);
+  };
+
   return (
     <div id="main">
-
-      <div id = "increment" onClick = {onClickIncrementHandler}>Increment</div>
-
+      <button id="increment" onClick={onClickIncrementHandler}>
+        Increment
+      </button>
 
       <div id="counter">
-
-        {getData % 3 === 0 && getData % 15 !== 0 && (<div className = "fizz">{getData}</div>)}
-
-        {getData % 5 === 0 && getData % 15 !== 0 && (<div className = "buzz">{getData}</div>)}
-
-        {getData % 15 === 0 && (<div className = "fizzbuzz">{getData}</div>)}
-
-        {getData % 3 !== 0 && getData % 5 !== 0 && getData % 15 !== 0 &&  (<div className = "normal">{getData}</div>)}
+        
+        {getData % 3 === 0 && getData % 15 !== 0 && (
+          <div className="fizz">{getData}</div>
+        )}
+        
+        {getData % 5 === 0 && getData % 15 !== 0 && (
+          <div className="buzz">{getData}</div>
+        )}
+        
+        {getData % 15 === 0 && <div className="fizzbuzz">{getData}</div>}
+        
+        {getData % 15 !== 0 && getData % 3 !== 0 && getData % 5 !== 0 && (
+          <div className="normal">{getData}</div>
+        )}
 
       </div>
 
-      <div id = "decrement" onClick = {onClickDecrementHandler}>Decrement</div>
-
+      <button id="decrement" onClick={onClickDecrementHandler}>
+        Decrement
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
