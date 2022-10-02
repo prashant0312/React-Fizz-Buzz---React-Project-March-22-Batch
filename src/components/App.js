97a38f6d4e80c8348dfbe96bde6d47fb882749c8
Prashant Madhukar Kale <61052188+prashant0312@users.noreply.github.com>
2022-10-02 12:10:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 import '../styles/App.css';
+
 const App = () => {
 
+  const [getData, setData] = useState(1);
+
+  const onClickIncrementHandler = () =>{
+    setData(getData + 1);
+  }
+
+  const onClickDecrementHandler = () =>{
+    setData(getData - 1);
+  }
   
   return (
     <div id="main">
+
+      <div id = "increment" onClick = {onClickIncrementHandler}>Increment</div>
+
+
+      <div id="counter">
+
+        {getData % 3 === 0 && getData % 15 !== 0 && (<div className = "fizz">{getData}</div>)}
+
+        {getData % 5 === 0 && getData % 15 !== 0 && (<div className = "buzz">{getData}</div>)}
+
+        {getData % 15 === 0 && (<div className = "fizzbuzz">{getData}</div>)}
+
+        {getData % 3 !== 0 && getData % 5 !== 0 && getData % 15 !== 0 &&  (<div className = "normal">{getData}</div>)}
+
+      </div>
+
+      <div id = "decrement" onClick = {onClickDecrementHandler}>Decrement</div>
 
     </div>
   )
