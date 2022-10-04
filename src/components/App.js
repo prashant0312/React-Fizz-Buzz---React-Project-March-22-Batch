4b277913bc442dd18b709a3f8abc48dfd62be882
Prashant Madhukar Kale <61052188+prashant0312@users.noreply.github.com>
2022-10-05 00:04:34 +0530

    FizzBuzz

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
-import '../styles/App.css';
-
-import React, { useState } from "react";
-import "./styles.css";
+import "../styles/App";
 
 const App = () => {
   const [getData, setData] = useState(1);
@@ -24,27 +21,38 @@ const App = () => {
       </button>
 
       <div id="counter">
-        {getData % 3 === 0 && (
-          <div className="fizz">{getData}</div>
-        )}
 
-        {getData % 5 === 0 && (
-          <div className="buzz">{getData}</div>
-        )}
+        {
+            getData % 3 === 0 && getData % 15 !== 0 && (
+              <div className="fizz">{getData}</div>
+        )
+        }
 
-        {getData % 15 === 0 && <div className="fizzbuzz">{getData}</div>}
+        {
+            getData % 5 === 0 && getData % 15 !== 0 && (
+              <div className="buzz">{getData}</div>
+        )
+        }
 
-        { getData % 3 !== 0 && getData % 5 !== 0 && (
-          <div className="normal">{getData}</div>
-        )}
+        {
+
+            getData % 15 === 0 && <div className="fizzbuzz">{getData}</div>
+        
+        }
+
+        {
+            getData % 15 !== 0 && getData % 3 !== 0 && getData % 5 !== 0 && (
+              <div className="normal">{getData}</div>
+        )
+        }
       </div>
-
+          
       <button id="decrement" onClick={onClickDecrementHandler}>
         Decrement
       </button>
+      
     </div>
   );
 };
 
 export default App;
-
