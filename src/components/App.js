5df01a61e3d59c05c99b26e343ff0e350aaae63a
Prashant Madhukar Kale <61052188+prashant0312@users.noreply.github.com>
2022-10-12 19:52:14 +0530

    fizzbuzz

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,28 @@ import React,{useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
+
   const[count,setCount] = useState(1);
   
-  const logic =()=>{
-  if(count%3===0 && count%5===0){
-    return "fizzbuzz"
-  }
-  else if(count%3===0){
-    return "fizz"
-  }
-  else if(count%5===0){
-    return "buzz"
-  }
-  else{
-    return "normal"
-  }
+  const logic = () => {
+      if(count % 3 === 0 && count % 5 === 0){
+        return "fizzbuzz";
+      }
+      else if(count % 3 === 0){
+        return "fizz";
+      }
+      else if(count % 5 === 0){
+        return "buzz";
+      }
+      else{
+        return "normal";
+      }
   };
     return (
       <div id="main">
-        <button id="increment" onClick={()=>setCount(count+1)}>increment</button>
-        <div id = "counter" className={logic()} >{count}</div>
-        <button id="decrement" onClick={()=>setCount(count-1)}>decrement</button>
+        <button id="increment" onClick={() => setCount(count + 1)}>increment</button>
+        <div id = "counter" className={logic()} > {count} </div>
+        <button id="decrement" onClick={() => setCount(count - 1)}>decrement</button>
       </div>
     )
     }
